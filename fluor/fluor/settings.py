0422a42cfcd03cf76eca8d93c10f0a6ba4d84b0b
Vorag1225 <snow05012008@gmail.com>
2026-09-19 11:16:11 +0300

    Настройка .env и локализации
    -Секретный ключ Django хранится в файле .env
    -Добавлен env.example с примером файла .env
    -Локализовал проект: поставил русский язык и перевёл время на московское

--- fluor/fluor/settings.py
+++ fluor/fluor/settings.py
@@ -9,8 +9,11 @@ https://docs.djangoproject.com/en/6.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
-
+from dotenv import load_dotenv
 from pathlib import Path
+import os
+
+load_dotenv() # Получает доступ к файлу .env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6g_e&twfee0j_n)d!fc(bpxod=lpqy@_cc2+cajl)v17ctf1&2'
+SECRET_KEY = os.getenv('SECRET_KEY') # Ключ хранится в файле .env
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -102,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC+3'
 
 USE_I18N = True
 
